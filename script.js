781def4bc9f7edc08fb707b94ea6d99fd2167071
Cheikh NDAO <cheikhndao@192.168.1.9>
2024-08-13 16:40:06 +0000

    2nde commit

---
+++ script.js
@@ -0,0 +1,114 @@
+// Initialize Swiper.js for Carousel
+var swiper = new Swiper('.swiper-container', {
+    speed: 1000,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// Initialize Swiper.js for Gallery
+var gallerySwiper = new Swiper('.gallery-slider', {
+    speed: 800,
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
+
+// Initialize Swiper.js for Partners
+var partnerSwiper = new Swiper('.partners-slider', {
+    speed: 800,
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 20,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+    },
+});
+
+// Counter Animation
+$('.counter').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+// AOS Initialization
+AOS.init();
+
+// GSAP Animations
+gsap.from(".navbar", { duration: 1.5, y: -100, opacity: 5, ease: "bounce" });
+
+gsap.from(".carousel-caption h1", {
+    duration: 5.5,
+    y: 50,
+    opacity: 10,
+    delay: Infinity,
+    ease: "power2.out"
+});
+
+// Change navbar background on scroll
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('#navbar').addClass('scrolled');
+        $('.nav-link').addClass('scrolled');
+        $('#navbar .navbar-brand img').attr('src', 'img/logo.png');
+
+        // Change navbar links color
+        // $('#navbar .nav-link').css('color', '#174fdd');
+    } else {
+        $('#navbar').removeClass('scrolled');
+    }
+});
